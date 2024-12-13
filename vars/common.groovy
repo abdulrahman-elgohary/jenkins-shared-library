748e695a193f14c1bf16cf6f5ff0cf3c142162a4
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-13 19:44:52 +0200

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,1 +1,8 @@
 
+def printMessage(String message) {
+         echo "Shared Library Message: ${message}"
+     }
+
+     def buildDockerImage(String imageName, String dockerfilePath = '.') {
+         sh "docker build -t ${imageName} ${dockerfilePath}"
+     }
