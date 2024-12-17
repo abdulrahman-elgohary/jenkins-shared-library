7ac4379236a33ec55f5162ff16bac607e241fcd6
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-17 11:32:08 +0200

    Update K8sDeployment.groovy

--- src/org/example/K8sDeployment.groovy
+++ src/org/example/K8sDeployment.groovy
@@ -1,1 +1,11 @@
+package org.example
 
+class K8sDeployment {
+    def deploy(String namespace, String environment) {
+        // Implement Kubernetes deployment logic
+        sh """
+            kubectl config use-context your-k8s-context
+            kubectl apply -f deployment.yaml -n ${namespace}
+        """
+    }
+}
