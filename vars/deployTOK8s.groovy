6fad30151565b4cd4c6f312c21f2bfc2fb08be18
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-17 11:31:10 +0200

    Update deployTOK8s.groovy

--- vars/deployTOK8s.groovy
+++ vars/deployTOK8s.groovy
@@ -1,1 +1,16 @@
-
+def call(Map config) {
+    pipeline {
+        agent any
+        
+        stages {
+            stage('Deploy to Namespace') {
+                steps {
+                    script {
+                        def k8sDeployer = new org.example.K8sDeployment()
+                        k8sDeployer.deploy(config.namespace, config.environment)
+                    }
+                }
+            }
+        }
+    }
+}
