71a4bc63d44638a5e681a349a471e50c2b956fd0
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-25 22:07:44 +0200

    Update sonarQubeTest.groovy

--- vars/sonarQubeTest.groovy
+++ vars/sonarQubeTest.groovy
@@ -1,10 +1,25 @@
 def call(Map envVars) {
+    // Git Checkout
+    echo 'Checking out repository...'
+    git branch: envVars.branch, url: envVars.repoUrl
+
+    // Unit Test
+    echo 'Running unit tests...'
+    dir('FinalProjectCode') {
+        sh '''
+        chmod +x ./gradlew
+        ./gradlew test
+        '''
+    }
+
     // SonarQube Test
     echo 'Running SonarQube analysis...'
     withSonarQubeEnv(envVars.sonarQubeServer) {
+        dir('FinalProjectCode') {
             sh '''
             ./gradlew clean build
-            ./gradlew sonar
+            ./gradlew sonarqube
             '''
+        }
     }
 }
