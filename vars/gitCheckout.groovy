17fa2938b831395d875e12a97d63d3a07b453e1c
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-25 22:15:27 +0200

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,4 @@
 def call(String repoUrl, String branch) {
+    echo 'Checking out repository...'
     checkout scm: [$class: 'GitSCM', branches: [[name: branch]], userRemoteConfigs: [[url: repoUrl]]]
 }
