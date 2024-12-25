ebdfa278b3e10559e1dcb5a43bc17df578389661
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-25 23:40:42 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,12 @@
 def call(Map envVars) {
     echo 'Building the Image from dockerfile...'
-    sh "docker build -t ${envVars.docker_hub_username}/${envVars.imageName}:${buildNumber} ."
+    sh "docker build -t ${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber} ."
+
+    echo 'Pushing Docker image to registry...'
+    withCredentials([usernamePassword(credentialsId: envVars.registryCredentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh """
+        echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+        docker push ${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber}
+        """
+    }
 }
