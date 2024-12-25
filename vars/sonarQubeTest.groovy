df8e9db5617920ad964e1d0c155d508413b95f18
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-25 21:34:42 +0200

    Update sonarQubeTest.groovy

--- vars/sonarQubeTest.groovy
+++ vars/sonarQubeTest.groovy
@@ -2,10 +2,8 @@ def call(Map envVars) {
     // SonarQube Test
     echo 'Running SonarQube analysis...'
     withSonarQubeEnv(envVars.sonarQubeServer) {
-        dir('FinalProjectCode') {
             sh '''
             ./gradlew sonarqube
             '''
-        }
     }
 }
