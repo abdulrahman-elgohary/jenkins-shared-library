a1730b5688f8cb3a766f09c48db8421f70df7851
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-25 21:50:40 +0200

    Update sonarQubeTest.groovy

--- vars/sonarQubeTest.groovy
+++ vars/sonarQubeTest.groovy
@@ -3,6 +3,7 @@ def call(Map envVars) {
     echo 'Running SonarQube analysis...'
     withSonarQubeEnv(envVars.sonarQubeServer) {
             sh '''
+            chmod +x 
             ./gradlew sonarqube
             '''
     }
