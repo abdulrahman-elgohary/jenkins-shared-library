2d948bda05be76315542bab8b2a7862e022213bb
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 22:46:32 +0200

    Update pushToGithub.groovy

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -1,1 +1,8 @@
-
+def call(Map envVars) {
+    echo 'Pushing the new deployment file to Github...'
+        sh """
+        git add .
+        git commit -m "Update Deployment File with new Image"
+        git push origin main
+        """
+}
