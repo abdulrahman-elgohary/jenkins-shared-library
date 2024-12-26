b362d4178e55934216b0d2dcf8969f2d26ad0573
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 23:52:57 +0200

    Update pushToGithub.groovy

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -1,12 +1,9 @@
 def call(Map envVars) {
-    // Extract parameters from the map with defaults
-    withCredentials(credentialsId: envVars.registryCredentials, branch: envVars.branch) {
     sh """
     git config user.name "${envVars.githubUsername}"
     git config user.email "${envVars.githubEmail}"
     git add .
-    git commit -m "Automated commit by Jenkins"
+    git commit -m "Automated commit by Jenkins" || echo "No changes to commit"
     git push origin ${envVars.branch}
     """
-    }
 }
