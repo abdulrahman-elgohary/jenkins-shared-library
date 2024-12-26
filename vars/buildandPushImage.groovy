5829b8dbeeb1872feda6c94afff8131cb2997996
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 18:05:13 +0200

    Update buildandPushImage.groovy

--- vars/buildandPushImage.groovy
+++ vars/buildandPushImage.groovy
@@ -3,10 +3,11 @@ def call(Map envVars) {
     dir('FinalProjectCode') {
     sh "docker build -t ${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber} ."
     }
-    echo 'Pushing Docker image to registry...'
+    echo 'Updating deployment file and Pushing Docker image to registry...'
     withCredentials([usernamePassword(credentialsId: envVars.registryCredentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
     dir('FinalProjectCode') {   
         sh """
+        sed -i 's|<IMAGE_PLACEHOLDER>|${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber}|g' deployment.yaml
         echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
         docker push ${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber}
         docker rmi ${envVars.docker_hub_username}/${envVars.imageName}:${envVars.buildNumber}
