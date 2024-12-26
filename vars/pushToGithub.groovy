6ed10aa954a4c80782e611dae1e91aabde3eae35
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 23:46:02 +0200

    Update pushToGithub.groovy

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -1,26 +1,12 @@
 def call(Map envVars) {
-    echo 'Pushing the new deployment file to Github...'
-    withCredentials([usernamePassword(credentialsId: envVars.registryCredentials, usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
-        sh """
-            # Configure git
-            git config --global user.email "${envVars.githubEmail}"
-            git config --global user.name "${envVars.githubUsername}"
-            
-            # Stash any changes first
-            git stash
-            
-            # Checkout main branch
-            git checkout main
-            
-            # Pop the stashed changes
-            git stash pop
-            
-            # Add and commit changes
-            git add .
-            git commit -m "Update Deployment File with new Image"
-            
-            # Push to main with credentials explicitly provided
-            git push https://github.com/${envVars.githubUsername}/${envVars.repoName}.git main
-        """
+    // Extract parameters from the map with defaults
+    withCredentials(credentialsId: envVars.registryCredentials, branch: envVars.branch) {
+    sh """
+    git config user.name "${envVars.githubUsername}"
+    git config user.email "${envVars.githubEmail}"
+    git add .
+    git commit -m "Automated commit by Jenkins"
+    git push origin ${envVars.branch}
+    """
     }
 }
