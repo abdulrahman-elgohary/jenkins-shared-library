3ac9294e674c305cabefb785d673e39165503094
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 23:57:03 +0200

    Update pushToGithub.groovy

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -2,6 +2,7 @@ def call(Map envVars) {
     sh """
     git config user.name "${envVars.githubUsername}"
     git config user.email "${envVars.githubEmail}"
+    git remote set-url origin ${envVars.gitRepo}
     git add .
     git commit -m "Automated commit by Jenkins" || echo "No changes to commit"
     git push origin ${envVars.branch}
