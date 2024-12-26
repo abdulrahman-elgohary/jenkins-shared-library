5b68f8d5fa9a3e1659379f577764aa9c21a00d23
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-12-26 23:09:03 +0200

    Update pushToGithub.groovy

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -2,13 +2,16 @@ def call(Map envVars) {
     echo 'Pushing the new deployment file to Github...'
     withCredentials([usernamePassword(credentialsId: envVars.registryCredentials, usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_PASSWORD')]) {
         sh """
-        git config --global user.email "enVars.githubEmail"
-        git config --global user.name "enVars.githubUsername"
-        git switch main
-        git add .
-        git commit -m "Update Deployment File with new Image"
-        git push origin main
+            git config --global user.email "${envVars.githubEmail}"
+            git config --global user.name "${envVars.githubUsername}"
+            
+            # Update the remote URL to include credentials
+            git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${envVars.githubUsername}/${envVars.repoName}.git
+            
+            git switch main
+            git add .
+            git commit -m "Update Deployment File with new Image"
+            git push origin main
         """
     }
 }
-    
